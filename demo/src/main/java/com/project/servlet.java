ecb948cff8c6448ab9f6963bfb5e32cdcdf86b9c
Christian Garo <54284709+chrischin555@users.noreply.github.com>
2023-10-08 19:14:46 -0700

    tried to create code to convert docx to pdf
    tried to code pdf converter but does not get updated through the demo.war file. try again later.

--- demo/src/main/java/com/project/servlet.java
+++ demo/src/main/java/com/project/servlet.java
@@ -1,45 +1,63 @@
-package com.project;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+public class servlet extends HttpServlet {
+    public void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // response.setContentType("text/html;carset-UTF-8");
+        // PrintWriter out = response.getWriter();
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public class servlet extends HttpServlet{
-    public void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        response.setContentType("text/html;carset-UTF-8");
-        PrintWriter out = response.getWriter();
         try {
-            //out = response.getWriter();
-            out.println("<html>");
-            out.println("<h1> Hello World!</h1>");
-            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "<h1>");
-            String user = request.getParameter("user");
-            out.println("<h2> Welcome " + user + "<h2>");
-            out.println("<html>");
+            Part filePart = request.getPart("wordFile"); // Assuming you have a form field named "wordFile" for file
+                                                         // upload
+
+            // Read the uploaded Word file
+            InputStream wordInputStream = filePart.getInputStream();
+            XWPFDocument wordDocument = new XWPFDocument(wordInputStream);
+
+            // Create a new PDF document
+            PDDocument pdfDocument = new PDDocument();
+            PDPage page = new PDPage();
+            pdfDocument.addPage(page);
+
+            // Extract text from wordDocument and write it to the PDF document
+            PDFont font = PDType1Font.HELVETICA_BOLD;
+            PDPageContentStream contentStream = new PDPageContentStream(pdfDocument, page);
+            contentStream.setFont(font, 12);
+            contentStream.beginText();
+            contentStream.newLineAtOffset(100, 700); // Adjust the coordinates as needed
+            for (XWPFParagraph paragraph : wordDocument.getParagraphs()) {
+                for (XWPFRun run : paragraph.getRuns()) {
+                    String text = run.getText(0);
+                    if (text != null) {
+                        contentStream.showText(text);
+                    }
+                }
+            }
+            contentStream.endText();
+            contentStream.close();
+
+            // Save the PDF to a file or send it as a response
+            pdfDocument.save(response.getOutputStream());
+            pdfDocument.close();
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        }
-        finally{
-            out.close();
+            // handles exceptions
         }
     }
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
 
     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    
 }
-
-
